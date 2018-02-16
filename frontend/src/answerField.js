ad36c43a467e81168a0d6291f640dbe4539a6cdd
Casey Primozic <me@ameo.link>
2018-02-15 18:03:54 -0600

    Simplified component and state organization
     * Rather than having two kinds of state, storing a name and label for each answer, and then storing the answer value separately from the data, we just store an array of question values
     * Got rid of the `PopulatedAnwerField` component in favor of a simplified setup overall
     * Added an `.eslintrc` file pre-set with good values for a React project
       * Installed `eslint` and `eslint-plugin-react` as dev dependencies
     * Added Ramda utility library

--- frontend/src/answerField.js
+++ frontend/src/answerField.js
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
-import {
-  Button,
-  FormControl,
-  ControlLabel,
-  FormGroup,
-  HelpBlock,
-} from "react-bootstrap";
+import React from "react";
 import "./App.css";
 import "./answerField.css";
 
 export const AnswerField = ({ value, label, onChange, name, placeholder }) => (
-  <div>
-    <p>{label}</p>
-    <input id="checkBox" type="checkbox" style={{width: 20}}/>
+  <div style={{ display: "flex", flexDirection: "row", paddingBottom: 5 }}>
+    <span style={{ display: "flex", alignSelf: "center", paddingRight: 4 }}>{label}</span>
+    <span style={{ paddingRight: 4 }}>
+      <input id="checkBox" type="checkbox" style={{width: 20}} />
+    </span>
     <input type='text' value={value} onChange={onChange} placeholder={placeholder} name={name} />
   </div>
-)
+);
